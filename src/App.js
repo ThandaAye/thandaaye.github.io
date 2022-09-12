2e399b88372b057e093d5558cd8167c3cf83d3e1
ThandaAye <thanda.aye03@gmail.com>
2022-09-12 19:46:38 +0800

    added main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import TypeWriter from 'react-typewriter';
+
+const mainBackground = new URL("../public/city-view.jpg", import.meta.url)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div >
+          <TypeWriter id='intro' typing={1}>Hello I'm Tracy</TypeWriter>
+          <img className='main-background' src={mainBackground}/>
+        </div>
       </header>
     </div>
   );
