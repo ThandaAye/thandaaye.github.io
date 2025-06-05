8dc4192b7ce9549bece6cc15d5ab603cb86683ce
ThandaAye <thanda.aye03@gmail.com>
2025-06-05 10:40:59 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
       <header>
-        <title>Thanda Aye | Portfolio</title>
+        <title>Thanda Aye @ Tracy| Portfolio</title>
         <nav class="navbar">
           <div class="container nav-container">
-            <div class="logo">Thanda Aye</div>
+            <div class="logo">Thanda Aye @ Tracy</div>
             <ul class="nav-links">
               <li><a href="#about">About</a></li>
               <li><a href="#skills">Skills</a></li>
-              <li><a href="#contact" class="contact-btn">Contact</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
         </nav>
